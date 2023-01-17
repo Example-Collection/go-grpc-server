f9dd528d6d49ef699f6b26dd7b89265e5df1557b
sang-w0o <robbyra@gmail.com>
2023-01-18 01:23:05 +0900

    chore: Update server

--- server.go
+++ server.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	pb "github.com/Example-Collection/go-grpc-server/proto"
 	"google.golang.org/grpc"
-	"io"
 	"log"
 	"net"
-	"time"
 )
 
 type personServiceServer struct {
@@ -16,75 +15,41 @@ type personServiceServer struct {
 	pb.UnimplementedPersonServiceServer
 
 	// 데이터베이스에 사용자가 저장된 것처럼 PersonRequest들을 담는 배열을 만든다.
-	savedPersons []*pb.PersonRequest
+	savedPersons []*pb.SavePersonRequest
 }
 
-func personRequestToPersonResponse(req *pb.PersonRequest) *pb.PersonResponse {
-	return &pb.PersonResponse{
-		Email:   req.Email,
-		Age:     req.Age,
-		Name:    req.Name,
-		Message: "Successfully saved!(name:" + req.Name + ")",
+func (s *personServiceServer) GetPersonInformation(_ context.Context, req *pb.GetPersonRequest) (*pb.GetPersonResponse, error) {
+	log.Printf("PersonRequest(email: %v) arrived.\n", req.Email)
+	// find person by email in savedPersons
+	var person *pb.SavePersonRequest
+	for _, p := range s.savedPersons {
+		if p.Email == req.Email {
+			person = p
+			break
+		}
 	}
+
+	if person == nil {
+		return nil, errors.New("person not found")
+	}
+
+	return &pb.GetPersonResponse{
+		Name:    person.Name,
+		Age:     person.Age,
+		Email:   person.Email,
+		Message: "Successfully found!(email:" + person.Email + ")",
+	}, nil
 }
 
-func (s *personServiceServer) GetPersonInformation(_ context.Context, req *pb.PersonRequest) (*pb.PersonResponse, error) {
+func (s *personServiceServer) SavePersonInformation(_ context.Context, req *pb.SavePersonRequest) (*pb.BasicMessageResponse, error) {
 	log.Printf("PersonRequest(name: %v, age: %d, email: %v, password: %v) arrived.\n", req.Name, req.Age, req.Email, req.Password)
 	s.savedPersons = append(s.savedPersons, req)
-	return personRequestToPersonResponse(req), nil
-}
+	return &pb.BasicMessageResponse{Message: "Successfully saved!(name:" + req.Name + ", email: " + req.Email + ")"}, nil
 
-func (s *personServiceServer) ListPersons(req *pb.ListPersonRequest, stream pb.PersonService_ListPersonsServer) error {
-	log.Printf("ListPersonRequest(email: %v) arrived.", req.Email)
-	for _, person := range s.savedPersons {
-		if person.Email == req.Email {
-			time.Sleep(time.Second) // Optional
-			if err := stream.Send(personRequestToPersonResponse(person)); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (s *personServiceServer) SavePersons(stream pb.PersonService_SavePersonsServer) error {
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			log.Println("Read all messages in client stream.\nClosing stream after sending response.")
-			return stream.SendAndClose(&pb.BasicResponse{Message: "All requests saved!"})
-		}
-		if err != nil {
-			log.Fatalf("%v.SavePersons(_) = _, %v", s, err)
-			return err
-		}
-		s.savedPersons = append(s.savedPersons, req)
-		log.Printf("Saved Person(name: %v, email: %v, age: %d).\n", req.Name, req.Email, req.Age)
-	}
-	return nil
-}
-
-func (s *personServiceServer) AskAndGetPersons(stream pb.PersonService_AskAndGetPersonsServer) error {
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			log.Println("Read all messages from client stream. Closing server stream.")
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-		log.Printf("PersonRequest(email: %v, name: %v, age: %d) arrived.", req.Email, req.Name, req.Age)
-		s.savedPersons = append(s.savedPersons, req)
-		time.Sleep(time.Second)
-		if err := stream.Send(personRequestToPersonResponse(req)); err != nil {
-			return err
-		}
-	}
 }
 
 func newServer() *personServiceServer {
-	savedPersons := []*pb.PersonRequest{
+	savedPersons := []*pb.SavePersonRequest{
 		{
 			Email:    "robbyra@gmail.com",
 			Age:      25,
